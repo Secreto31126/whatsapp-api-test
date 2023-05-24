8d50f736e9d1389f335a67f058489b45a2627e6d
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-05-24 14:02:56 -0300

    Initial commit

---
+++ api/index.ts
@@ -0,0 +1,53 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import WhatsAppAPI from "whatsapp-api-js/index";
+import { Node18 } from "whatsapp-api-js/setup/node";
+import type { GetParams } from "whatsapp-api-js/types";
+
+const Whatsapp = new WhatsAppAPI(Node18({
+    token: process.env.WHATSAPP_TOKEN,
+    appSecret: process.env.WHATSAPP_APP_SECRET,
+    webhookVerifyToken: process.env.WHATSAPP_WEBHOOK_VERIFY_TOKEN
+}));
+
+Whatsapp.on.message = console.log;
+
+export default async function (req: VercelRequest, res: VercelResponse) {
+    if (req.method == "POST") {
+        console.log("Headers", req.headers["x-hub-signature-256"]);
+
+        const chunks = [];
+        req.on('data', (chunk) => {
+            chunks.push(chunk);
+        }).on('end', () => {
+            const body = Buffer.concat(chunks).toString();
+
+            console.log("Body", body);
+
+            try {
+                Whatsapp.post(JSON.parse(body), body, req.headers["x-hub-signature-256"] as string);
+            } catch (error) {
+                return res.status(error).send("Error");
+            }
+
+            return res.status(200).send("Success");
+        }).on("error", () => {
+            return res.status(500).send("Error");
+        });
+    }
+
+    else if (req.method == "GET") {
+        try {
+            console.log(req.query);
+        } catch (error) {
+            return res.status(400).send("Error");
+        }
+
+        try {
+            return res.status(200).send(Whatsapp.get(req.query as GetParams));
+        } catch (error) {
+            return res.status(error).send("Error");
+        }
+    }
+
+    else return res.status(501).send("Error");
+}
