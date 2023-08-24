49ad8a4684f5ff7edabd5f25ce9daf70672ddc10
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-08-24 20:37:15 -0300

    Tweaks

--- tests/test.ts
+++ tests/test.ts
@@ -5,19 +5,20 @@ test.describe('Test if the buttons work', () => {
 		test(`Button ${i + 1} works`, async ({ page }) => {
 			await page.goto('/');
 			await page.waitForTimeout(500);
+
 			await page.getByPlaceholder('Token').fill(process.env.TOKEN as string);
 			await page.getByPlaceholder('From (phoneID)').fill(process.env.FROM as string);
 			await page.getByPlaceholder('To (phone)').fill(process.env.TO as string);
 
 			const button = (await page.$$(`button`))[i];
 			if (!button) throw new Error('Button not found');
-
 			await button.click();
-			expect(page.url()).toBe('http://localhost:5173/');
+
 			const response = await page.waitForResponse('https://graph.facebook.com/**', {
 				timeout: 5000
 			});
 			expect(response.ok()).toBeTruthy();
+
 			await page.waitForTimeout(1000);
 		});
 	}
